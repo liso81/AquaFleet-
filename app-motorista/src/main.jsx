7ebae002ae9a8caa53236e730ab3740bfa071e5c
liso81 <rodriguezlisandromiguel@gmail.com>
2026-07-14 14:59:10 +0100

    Update main.jsx

--- app-motorista/src/main.jsx
+++ app-motorista/src/main.jsx
@@ -1,10 +1,10 @@
- import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AdminMotoristas from "../AdminMotoristas.jsx";
+import App from "../App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AdminMotoristas />
+    <App />
   </React.StrictMode>
-);
+); 
