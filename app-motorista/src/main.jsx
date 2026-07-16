13c82b0b754dd03631539e820f37b1139ca35148
liso81 <rodriguezlisandromiguel@gmail.com>
2026-07-16 07:15:38 +0100

    Update main.jsx

--- app-motorista/src/main.jsx
+++ app-motorista/src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "../App.jsx";
+import AdminGate from "../AdminGate.jsx";
+
+const esAdmin = window.location.pathname.startsWith("/admin");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {esAdmin ? <AdminGate /> : <App />}
   </React.StrictMode>
-); 
+);
